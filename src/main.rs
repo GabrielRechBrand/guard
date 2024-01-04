73b7ed2eea64d00f6466ac9c78e7a4ac67311d97
GabrielRechBrand <rcgabrielbrand@gmail.com>
2024-01-04 00:33:21 -0300

    master - Starting WebSocket integration and CPU Usage sharing.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
-use sysinfo::{
-    System,
-};
+use std::{net::TcpListener, thread::spawn};
+use tungstenite::{accept_hdr, handshake::server::{Request, Response}, Message};
+use sysinfo::{System};
 
+fn main () {
+    let server = TcpListener::bind("127.0.0.1:8080").unwrap();
+    for stream in server.incoming() {
+        spawn(move || {
+            let mut sys = System::new_all();
+            sys.refresh_all();
 
+            let callback = |req: &Request, response: Response| {
+                println!("Received a new ws handshake");
+                println!("The request's path is: {}", req.uri().path());
 
-use std::thread;
-use std::time::Duration;
+                Ok(response)
+            };
 
-fn main() {
-    let mut system = System::new_all();
+            let mut websocket = accept_hdr(stream.unwrap(), callback).unwrap();
+            let welcome_message = Message::Text("Connected to Guard".to_string());
+            websocket.send(welcome_message).unwrap();
 
-    loop {
-        system.refresh_all();
+            loop {
+                sys.refresh_cpu();
 
-        println!("-----------------PROCESSOR------------------");
-        for cpu in system.cpus() {
-            println!("{} {}%", cpu.name(), (cpu.cpu_usage() * 100.0).round() / 100.0);
-        }
-        println!("--------------------------------------------");
+                for cpu in sys.cpus() {
+                    let cpu_usage = Message::Text(cpu.cpu_usage().to_string());
+                    websocket.send(cpu_usage).unwrap();
+                }
 
-        println!(" ");
-
-        println!("-------------------MEMORY--------------------");
-        println!(
-            "Memory Usage: {} MB / {} MB",
-            system.used_memory() / 1024,
-            system.total_memory() / 1024
-        );
-        println!("--------------------------------------------");
-
-        thread::sleep(Duration::from_secs(5));
+                std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+            }
+        });
     }
 }
